abbf558b892188cffa372cb7c944010a97843f0e
Marta Lewandowska <mlewando@redhat.com>
2025-03-14 14:20:04 +0100

    set variables correctly

--- utils.mk
+++ utils.mk
@@ -10,12 +10,12 @@ RELEASE = 1
 DESTDIR := temp
 DATE=$(shell date "+%Y%m%d")
 ESPDIR := /boot/efi/EFI/BOOT
-KVRA := $(shell rpm -q kernel-core --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' | tail -n 1)
+KVRA := $(shell ls -1 /lib/modules | tail -1)
 ARCH := $(shell rpm --eval '%{_build_arch}')
 EFI_ARCH := $(shell rpm --eval '%{efi_arch}')
 OS_NAME := $(shell grep '^ID=' /etc/os-release | sed 's/ID=//')
 OS_VERSION := $(shell grep '^VERSION_ID=' /etc/os-release | sed 's/VERSION_ID=//')
-OS_DIST := $(shell rpm --eval '%{dist}')
+OS_DIST=$(shell ls -1 /lib/modules | grep -Eo '\.[cefln]+[0-9]+' | tail -n1)
 VR := $(VERSION)-$(RELEASE)$(OS_DIST)
 
 ifeq ($(.DEFAULT_GOAL),)
